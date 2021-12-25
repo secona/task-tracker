62abc91b2dc7fa502acfb03fa28a5bda38f422fa
muvs <77039267+muvs@users.noreply.github.com>
2021-12-25 12:01:50 +0700

    `tasks` route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,10 +2,11 @@ import { Express } from 'express';
 import users from './routes/users';
 import auth from './routes/auth';
 import projects from './routes/projects';
+import tasks from './routes/tasks';
 
 export default (app: Express) => {
   app.use('/api/auth', auth);
   app.use('/api/users', users);
   app.use('/api/projects', projects);
-  // tasks not yet implemented
+  app.use('/api/tasks', tasks)
 };
