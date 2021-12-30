3d0d34947d8fe81ab065dea6120dc9c47ddc9ddf
muvs <77039267+muvs@users.noreply.github.com>
2021-12-30 16:27:52 +0700

    add new project route

--- server/src/routes/me/projects.ts
+++ server/src/routes/me/projects.ts
@@ -1,4 +1,11 @@
 import { Router } from 'express';
+import { projectDAO } from '~/core/projects/project.dao';
+import {
+  ProjectInsert,
+  projectValidation,
+} from '~/core/projects/project.model';
+import authenticate from '~/middlewares/authenticate';
+import validateBody from '~/middlewares/validateBody';
 
 const router = Router();
 
@@ -6,8 +13,24 @@ router.get('/', (req, res) => {
   res.sendStatus(501);
 });
 
-router.post('/', (req, res) => {
-  res.sendStatus(501);
+router.post('/', authenticate, validateBody(projectValidation), (req, res) => {
+  const data = new ProjectInsert({
+    ...(req.parsedBody as ProjectInsert),
+    user_id: req.accessToken.user_id,
+  });
+
+  projectDAO
+    .create(data)
+    .then(project => {
+      res.status(201).json({
+        success: true,
+        data: { project },
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.json({ err });
+    });
 });
 
 export default router;
