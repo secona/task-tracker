58f965e601b40133ba5f10d0e091e7de2ba5322f
muvs <77039267+muvs@users.noreply.github.com>
2022-01-19 17:12:35 +0700

    get projects tasks

--- server/src/routes/projects/tasks/index.ts
+++ server/src/routes/projects/tasks/index.ts
@@ -1,10 +1,31 @@
-import { Router } from 'express';
+import { Request, Router } from 'express';
+import db from '~/db';
+import authenticate from '~/middlewares/authenticate';
 
-const router = Router();
+const router = Router({ mergeParams: true });
 
-router.post('/', (req, res) => {
-  res.sendStatus(501);
-});
+router
+  .route('/')
+  .all(authenticate)
+
+  .post((req, res) => {
+    res.sendStatus(501);
+  })
+
+  .get((req: Request<any>, res) => {
+    db('tasks')
+      .select('tasks.*')
+      .leftJoin('projects', { 'projects.project_id': 'tasks.project_id' })
+      .where({ 'tasks.project_id': req.params.projectId })
+      .andWhere({ 'projects.user_id': req.accessToken.user_id })
+      .then(tasks => {
+        res.json({ data: { tasks } });
+      })
+      .catch(err => {
+        console.error(err);
+        res.json({ err });
+      });
+  });
 
 router
   .route('/:taskId')
