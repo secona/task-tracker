28db10e4b7f243ea01f291cfdbb4e99d6f5a6e27
muvs <77039267+muvs@users.noreply.github.com>
2022-01-19 13:22:50 +0700

    get all tasks route

--- server/src/routes/me/tasks.ts
+++ server/src/routes/me/tasks.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import db from '~/db';
+import authenticate from '~/middlewares/authenticate';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.sendStatus(501);
+router.get('/', authenticate, (req, res) => {
+  db('tasks')
+    .select('tasks.*')
+    .leftJoin('projects', { 'projects.project_id': 'tasks.project_id' })
+    .where({ 'projects.user_id': req.accessToken.user_id })
+    .then(tasks => {
+      res.status(200).json({ data: { tasks } });
+    })
+    .catch(err => {
+      console.error(err);
+      res.json({ err });
+    });
 });
 
 export default router;
