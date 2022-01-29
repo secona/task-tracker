0300ccd3c6b89c7dc8e19bf09e638b106142f493
muvs <77039267+muvs@users.noreply.github.com>
2022-01-29 16:04:58 +0700

    project's tasks crud

--- server/src/routes/projects/tasks/index.ts
+++ server/src/routes/projects/tasks/index.ts
@@ -1,6 +1,10 @@
 import { Request, Router } from 'express';
 import { taskDAO } from '~/core/tasks/task.dao';
-import { TaskInsert, taskValidation } from '~/core/tasks/task.model';
+import {
+  TaskInsert,
+  TaskUpdate,
+  taskValidation,
+} from '~/core/tasks/task.model';
 import authenticate from '~/middlewares/authenticate';
 import authorize from '~/middlewares/authorize';
 import validateBody from '~/middlewares/validateBody';
@@ -40,17 +44,56 @@ router
 
 router
   .route('/:taskId')
+  .all(authenticate, authorize.project)
 
-  .get((req, res) => {
-    res.sendStatus(501);
+  .get((req: Request<any>, res) => {
+    taskDAO
+      .getOne({
+        task_id: req.params.taskId,
+        project_id: req.params.projectId,
+      })
+      .then(task => {
+        res.json({ data: { task } });
+      })
+      .catch(err => {
+        console.error(err);
+        res.json({ err });
+      });
   })
 
-  .patch((req, res) => {
-    res.sendStatus(501);
+  .patch(validateBody(taskValidation), (req: Request<any>, res) => {
+    taskDAO
+      .update(
+        {
+          task_id: req.params.taskId,
+          project_id: req.params.projectId,
+        },
+        req.parsedBody as TaskUpdate
+      )
+      .then(task => {
+        res.json({ data: { task } });
+      })
+      .catch(err => {
+        console.error(err);
+        res.json({ err });
+      });
   })
 
-  .delete((req, res) => {
-    res.sendStatus(501);
+  .delete((req: Request<any>, res) => {
+    taskDAO
+      .del({
+        task_id: req.params.taskId,
+        project_id: req.params.projectId,
+      })
+      .then(n => {
+        const success = n > 0;
+        res.status(success ? 200 : 404);
+        res.json({ success });
+      })
+      .catch(err => {
+        console.error(err);
+        res.json({ err });
+      });
   });
 
 export default router;
