09bdee42a05657843f4ec5e5998c83bab05a3c06
muvs <77039267+muvs@users.noreply.github.com>
2022-05-09 13:22:00 +0700

    update env

--- server/types/env.d.ts
+++ server/types/env.d.ts
@@ -1,12 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    GOOGLE_CLIENT_ID: string;
-    GOOGLE_CLIENT_SECRET: string;
+    NODE_ENV: string;
     DATABASE_URL: string;
     ROOT_URL: string;
-    NODE_ENV: string;
-    ACCESS_TOKEN_SECRET: string;
     VERIFICATION_TOKEN_SECRET: string;
+    EMAIL_SERVICE: string;
     EMAIL_ADDRESS: string;
     EMAIL_PASSWORD: string;
     EMAIL_NAME: string;
