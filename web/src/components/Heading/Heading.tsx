5f168d2396c493b75717880f9e89be409e3cba7a
secona <77039267+secona@users.noreply.github.com>
2022-08-19 21:35:27 +0700

    set heading font-weight
    to bold or 700

--- web/src/components/Heading/Heading.tsx
+++ web/src/components/Heading/Heading.tsx
@@ -12,6 +12,7 @@ export interface HeadingProps
 
 export const Heading = styled.h1<StrippedHeadingProps>`
   line-height: 1;
+  font-weight: 700;
   font-size: ${p => p.theme.fontSizes[p.fontSize || 'md']} !important;
   color: ${p => p.theme.color[p.fontColor || 'white']};
   font-size: 1rem;
