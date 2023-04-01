3b911ec798dcb79b4b3f26526596d513ba99766a
secona <77039267+secona@users.noreply.github.com>
2023-04-01 21:12:59 +0700

    Dashboard: style SidebarItem

--- web/src/components/Dashboard/Sidebar/SidebarItem.tsx
+++ web/src/components/Dashboard/Sidebar/SidebarItem.tsx
@@ -12,7 +12,9 @@ interface SidebarItemProps {
 export const SidebarItem = (props: SidebarItemProps) => {
   return (
     <NavLink
+      role='button'
       to={props.to}
+      draggable={false}
       className={({ isActive }) =>
         [
           'dashboard__sidebar__item',
@@ -20,6 +22,8 @@ export const SidebarItem = (props: SidebarItemProps) => {
         ].join(' ')
       }
     >
+      <props.Icon className='dashboard__sidebar__item__icon' size={16} />
+      {/* <span>{props.label}</span> */}
       {props.label}
     </NavLink>
   );
