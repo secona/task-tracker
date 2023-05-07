34fc8b7647c8f634300b8ab7ec25c5439000bc1d
secona <77039267+secona@users.noreply.github.com>
2023-05-07 14:23:09 +0700

    migrate storybook to v7
    * add typings to main.js and preview.js in .storybook

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,13 +1,14 @@
-module.exports = {
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
   features: {
     storyStoreV7: true,
@@ -17,3 +18,5 @@ module.exports = {
     return config;
   },
 };
+
+export default config;
--- web/.storybook/preview.js
+++ web/.storybook/preview.js
@@ -2,6 +2,7 @@ import { globalVariables } from '../src/styles/global';
 
 import '../src/styles/global.scss';
 
+/** @type { import('@storybook/react').Preview } */
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
