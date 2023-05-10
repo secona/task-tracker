839db6659b45e271af8e28d6d61f2480ec14dd4c
secona <77039267+secona@users.noreply.github.com>
2023-05-10 18:02:17 +0700

    poopulate inbox with dummy tasks
    * adjust task gap and padding for color coded tasks

--- web/src/pages/Inbox.tsx
+++ web/src/pages/Inbox.tsx
@@ -1,5 +1,71 @@
+import { Heading } from '@/components/Heading';
+import { Task } from '@/components/Task/Task';
+
 export const Inbox = () => {
   return (
-    <>your tasks</>
+    <>
+      <Heading fontSize='6xl'>Good Morning</Heading>
+      <div className='project__task-group'>
+        <p className='project__task-group__title'>Unfinished</p>
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+        />
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+        />
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+        />
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+        />
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+        />
+      </div>
+      <div className='project__task-group'>
+        <p className='project__task-group__title'>Finished</p>
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+          done={true}
+        />
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+          done={true}
+        />
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+          done={true}
+        />
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+          done={true}
+        />
+        <Task
+          task='Math Homework'
+          description='Page 11 Exercise 2'
+          colorCode={0}
+          done={true}
+        />
+      </div>
+    </>
   );
-}
+};
