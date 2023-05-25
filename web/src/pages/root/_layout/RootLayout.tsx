fcee4e9f732f68d59a68f0a1005da11d324dae69
secona <77039267+secona@users.noreply.github.com>
2023-05-25 08:54:45 +0700

    move Dashboard to RootLayout

--- web/src/pages/root/_layout/RootLayout.tsx
+++ web/src/pages/root/_layout/RootLayout.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from 'react-router-dom';
-import { Dashboard } from '@/components/Dashboard';
+import { Topbar } from './Topbar';
+import { Sidebar } from './Sidebar';
+
+import './RootLayout.scss';
 
 export const RootLayout = () => {
   return (
-    <Dashboard>
-      <Outlet />
-    </Dashboard>
+    <div className='dashboard'>
+      <Topbar />
+      <div className='dashboard__main'>
+        <Sidebar />
+        <div className='dashboard__content'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
   );
 };
