029db60da531e9b7691c81b0e0cda50b2fd06565
secona <77039267+secona@users.noreply.github.com>
2023-05-26 16:30:33 +0700

    feat(web): display all user's tasks in inbox

--- web/src/pages/root/Inbox.tsx
+++ web/src/pages/root/Inbox.tsx
@@ -2,69 +2,57 @@ import { Heading } from '@/components/Heading';
 import { Task, TaskGroup } from '@/components/Task';
 
 import './Project.scss';
+import { useQueries } from '@tanstack/react-query';
+import { projectsListQuery } from './_layout';
+import { tasksQuery } from './Project';
+import { separateTasks } from '@/utils/separateTasks';
 
 export const Inbox = () => {
+  const [projectsQ, tasksQ] = useQueries({
+    queries: [projectsListQuery, tasksQuery()],
+  });
+
+  // @ts-ignore
+  const tasks = separateTasks(tasksQ.data?.data.tasks);
+
   return (
     <div className='project'>
       <Heading fontSize='6xl'>Good Morning!</Heading>
       <TaskGroup title='Unfinished'>
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-        />
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-        />
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-        />
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-        />
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-        />
+        {(() => {
+          if (projectsQ.data?.data.msg === 'SUCCESS') {
+            const projects = projectsQ.data.data.projects;
+            return tasks.unfinished.map(task => (
+              <Task
+                task={task.task}
+                description={task.description}
+                colorCode={
+                  projects.find(
+                    project => project.project_id === task.project_id
+                  )?.color || 0
+                }
+              />
+            ));
+          }
+        })()}
       </TaskGroup>
       <TaskGroup title='Finished'>
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-          done={true}
-        />
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-          done={true}
-        />
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-          done={true}
-        />
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-          done={true}
-        />
-        <Task
-          task='Math Homework'
-          description='Page 11 Exercise 2'
-          colorCode={0}
-          done={true}
-        />
+        {(() => {
+          if (projectsQ.data?.data.msg === 'SUCCESS') {
+            const projects = projectsQ.data.data.projects;
+            return tasks.finished.map(task => (
+              <Task
+                task={task.task}
+                description={task.description}
+                colorCode={
+                  projects.find(
+                    project => project.project_id === task.project_id
+                  )?.color || 0
+                }
+              />
+            ));
+          }
+        })()}
       </TaskGroup>
     </div>
   );
