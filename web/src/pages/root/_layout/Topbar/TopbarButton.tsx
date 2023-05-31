40c195d24f4036db4ad0cbc44b348dd1fb4d737a
secona <77039267+secona@users.noreply.github.com>
2023-05-31 16:17:13 +0700

    fix(web): TopbarButton onClick not working
    * forgot to pass TopbarButton props to the internal button

--- web/src/pages/root/_layout/Topbar/TopbarButton.tsx
+++ web/src/pages/root/_layout/Topbar/TopbarButton.tsx
@@ -2,18 +2,18 @@ import { ComponentPropsWithRef, forwardRef } from 'react';
 import { Icon } from 'react-feather';
 
 import topbarButtonCN from './TopbarButton.module.scss';
+import { cnProps } from '@/utils/mergeClassnames';
 
 export interface TopbarButtonProps extends ComponentPropsWithRef<'button'> {
   Icon: Icon;
 }
 
 export const TopbarButton = forwardRef<HTMLButtonElement, TopbarButtonProps>(
-  (props, ref) => {
+  ({ Icon, ...props }, ref) => {
     return (
-      <button className={topbarButtonCN.topbarButton} ref={ref}>
-        <props.Icon size={16} color='white' />
+      <button {...cnProps(props, topbarButtonCN.topbarButton)} ref={ref}>
+        <Icon size={16} color='white' />
       </button>
     );
   }
 );
-
