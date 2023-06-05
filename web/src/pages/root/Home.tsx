ad9a525976200b28312efd91383fe1c92cd6e05f
secona <77039267+secona@users.noreply.github.com>
2023-06-05 10:24:57 +0700

    refactor(web): edit Home to use the new api
    * remove QueryState from Home
    
    * remove ProjectGrid and merging it to Home
    
    * remove checking for SUCCESS in query data

--- web/src/pages/root/Home.tsx
+++ web/src/pages/root/Home.tsx
@@ -1,34 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
-import { QueryState } from '@/components/QueryState';
 import { queries } from '@/queries';
 import { Content } from './_layout';
 
 export const Home = () => {
+  const query = useQuery(queries.projects());
+
   return (
     <Content.Container>
       <Content.Card>
         <Content.ProjectName>Good Morning!</Content.ProjectName>
       </Content.Card>
-      <QueryState
-        Error={({ resetErrorBoundary }) => (
-          <button onClick={resetErrorBoundary}>retry</button>
-        )}
-      >
-        <ProjectGrid />
-      </QueryState>
-    </Content.Container>
-  );
-};
-
-const ProjectGrid = () => {
-  const query = useQuery(queries.projects());
-
-  return (
-    <Content.ProjectGrid>
-      {query.data?.data.msg === 'SUCCESS' &&
-        query.data.data.projects.map(project => (
+      <Content.ProjectGrid>
+        {query.data?.data.projects.map(project => (
           <Content.Project key={project.project_id} project={project} />
         ))}
-    </Content.ProjectGrid>
+      </Content.ProjectGrid>
+    </Content.Container>
   );
 };
