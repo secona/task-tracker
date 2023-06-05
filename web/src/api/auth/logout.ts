be9a5fe76b47b3a1655619f8a819cc68d8e24343
secona <77039267+secona@users.noreply.github.com>
2023-06-05 13:15:57 +0700

    refactor(web): edit logout to use the new API pattern
    * add defaultOptions to mutations of true for useErrorBoundary

--- web/src/api/auth/logout.ts
+++ web/src/api/auth/logout.ts
@@ -1,17 +1,17 @@
 import axios from 'axios';
-import { BaseAPI, ResponseBody } from '..';
+import { BaseAPI, NEW_ResponseBody, NEW_axios } from '..';
 
 export interface AuthLogoutContext {}
 
 export interface AuthLogoutBody {}
 
-export type AuthLogoutResponse = ResponseBody;
+export type AuthLogoutResponse = NEW_ResponseBody;
 
 export interface AuthLogoutAPI
   extends BaseAPI<AuthLogoutContext, AuthLogoutBody, AuthLogoutResponse> {}
 
 const logout: AuthLogoutAPI = () => {
-  return axios.post<AuthLogoutResponse>('/api/auth/logout');
+  return NEW_axios.post<AuthLogoutResponse>('/api/auth/logout');
 };
 
 export default logout;
