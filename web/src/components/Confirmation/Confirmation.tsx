5e008e5a6712bba49d622a82fc00342f093d20b3
secona <77039267+secona@users.noreply.github.com>
2023-07-10 10:44:38 +0700

    fix(web): Confirmation not having a background
    * add flex to Modal.Footer

--- web/src/components/Confirmation/Confirmation.tsx
+++ web/src/components/Confirmation/Confirmation.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button } from '../Button';
 import { ModalContent } from '../Modal/ModalContent';
+import { Modal } from '../Modal/Modal';
+import { Check, X } from 'react-feather';
 
 export interface ConfirmationProps {
   children: React.ReactNode;
@@ -25,22 +27,29 @@ export const Confirmation = ({
     <>
       {open && (
         <ModalContent closeModal={() => setOpen(false)}>
-          {children}
-          <Button
-            onClick={() => {
-              onYes();
-              setOpen(false);
-            }}
-          >
-            Yes
-          </Button>
-          <Button
-            onClick={() => setOpen(false)}
-            variant='secondary'
-            ref={el => setNoBtn(el!)}
-          >
-            No
-          </Button>
+          <Modal.Main>
+            <Modal.Title>Confirm</Modal.Title>
+            {children}
+          </Modal.Main>
+          <Modal.Footer>
+            <Button
+              LeftIcon={Check}
+              onClick={() => {
+                onYes();
+                setOpen(false);
+              }}
+            >
+              Yes
+            </Button>
+            <Button
+              LeftIcon={X}
+              onClick={() => setOpen(false)}
+              variant='secondary'
+              ref={el => setNoBtn(el!)}
+            >
+              No
+            </Button>
+          </Modal.Footer>
         </ModalContent>
       )}
     </>
